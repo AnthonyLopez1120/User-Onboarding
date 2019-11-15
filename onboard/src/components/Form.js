e1df3be9763e6f59d5ad789b0d5a6077a0daef06
Anthony Lopez <anthonybeconscious@gmail.com>
2019-11-14 19:20:41 -0600

    MVP

--- onboard/src/components/Form.js
+++ onboard/src/components/Form.js
@@ -1,19 +1,24 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import axios from "axios"
 import { withFormik, Form, Field } from "formik"
 import * as Yup from "yup"
 
-function FormX ({values}){
+function FormX ({values, errors, touched, status}){
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        status && setUsers([...users, status])}, [status, users]);
 
     return(
         <Form>
+            {touched.name&&errors.name}
             <Field type = "text" name= "name" placeholder ="Name" />
             <Field type = "email" name= "email" placeholder ="Email" />
             <Field type = "text" name= "password" placeholder ="Password" />
             
-           <p className = "terms">Terms of Service<br/> By clicking here you agree to forfiet all earthly possessions and   <Field type = "checkbox" name="terms" checked={values.terms} /></p>
+           <p className = "terms">Terms of Service<br/> By clicking here you agree to forfiet all earthly possessions and dedicate your life to servitude   <Field type = "checkbox" name="terms" checked={values.terms} /></p>
 
-        <button>Submit</button>
-         
+        <button type = "submit">Submit</button>
+
 
         </Form>
     )
@@ -30,19 +35,39 @@ const FormikLoginForm = withFormik({
       
     },
     validationSchema: Yup.object().shape({
-        email: Yup.string().min(3, "Name must be longer than three characters").required("Name is required")
-          .email()
-          .required(),
+        name: Yup.string()
+        .min(3, "Name must be longer than three characters").required("Name is required"),
+        
+        email: Yup.string()
+          .email("Email invalid")
+          .required("Email is required"),
+          
         password: Yup.string()
-          .min(6)
-          .required()
+          .min(6, "Password needs to be longer than 6 characteres")
+          .required("Password is required")
       }),
-      handleSubmit(values) {
-        console.log(values);
 
-      }
-})
-(FormX)
+      handleSubmit(values, { resetForm, setSubmitting, setStatus, setErrors }) {
+        console.log(values);
+        values.email === "alphabeta@kappa.com"
+        ? setErrors({email: "That email is already taken."})
+    
+        : axios
+        .post("https://reqres.in/api/users", values)
+        .then(response => {
+            console.log("Success!", response);
+            let user = response.data;
+            setStatus(user);
+            resetForm();
+            setSubmitting(false);
+            setStatus();
+        })
+        .catch(error => {
+            console.log("Mission failed.", error);
+            setSubmitting(false);
+        });
+      }    
+})(FormX)
 
 
 export default FormikLoginForm
